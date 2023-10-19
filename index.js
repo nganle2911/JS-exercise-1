4da7b1144cb5c26bcf87af96dde0419462df1535
Ngan LE <ngan.ltkn2911@gmail.com>
2023-10-19 11:35:20 +0200

    converted money

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /**
- * 3-block Model - Exercise 1
+ * TODO: 3-block Model - Exercise 1
  * Input:
  *  - salary per day
  *  - number of working days 
  * Logic Block:
  *  - Step 1: Create 2 variables for salary per day & number of working days
- *  - Step 2: calculate salary with formula: salary = salary per day * number of working days
+ *  - Step 2: Calculate salary with formula: salary per day * number of working days
  *  - Step 3: Print out the result 
  * Output: 
  *  - salary 
@@ -25,10 +25,15 @@ function calculateSalary() {
 };
 
 /**
- * 3-block Model - Exercise 2 
+ * TODO: 3-block Model - Exercise 2 
  * Input: 
+ *  - 5 numbers 
  * Logic Block:
+ *  - Step 1: Create 5 variables for 5 numbers
+ *  - Step 2: Calculate the average number with formula: (num1 + ... + num5) / 5
+ *  - Step 3: Print out the result 
  * Output: 
+ *  - the average number
  */
 
 function calAverageNum() {
@@ -46,4 +51,29 @@ function calAverageNum() {
     document.getElementById("ex2-output").innerText = averageNum; 
 }; 
 
+/**
+ * TODO: 3-block model - Exercise 3
+ * Input:
+ *  - amount of money in USD
+ *  - exchange rate 1USD = 23500VND 
+ * Logic Block:
+ *  - Step 1: Create a constant of exchange rate 1 USD = 23500VND
+ *  - Step 2: Create a variable for amount of money in USD
+ *  - Step 3: Calculate money after converting from USD to VND with formula: amount of money in USD * exchange rate
+ *  - Step 4: Print out the result 
+ * Output:
+ *  - converted money 
+ */
 
+function convertMoney() {
+    // Constant of exchange rate 
+    const EX_RATE = 23500;
+    // Input amount 
+    var amountEl = document.getElementById("amount").value * 1;
+
+    // Calculate the converted amount 
+    var convertedMoney = amountEl * EX_RATE; 
+
+    // Print out the result 
+    document.getElementById("ex3-output").innerText = convertedMoney; 
+}; 
